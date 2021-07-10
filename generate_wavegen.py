dbf4e7ad1fe32edeb261a3bbc917e4fa70ec6b21
Brad Hesson <brad.hesson@outlook.com>
2021-07-09 20:10:16 -0600

    added black formatter and formatted code

--- generate_wavegen.py
+++ generate_wavegen.py
@@ -4,32 +4,33 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
 
-dt = 0.01 # seconds
-ramp_rates = linspace(10, 0.1, 21) # volts/seconds  !!number must be odd!!
-displacements = linspace(5, 90, 21)/20 # volts  !!number must be odd!!
-hold_time = 5*60 # seconds
+dt = 0.01  # seconds
+ramp_rates = linspace(10, 0.1, 21)  # volts/second  !!number must be odd!!
+displacements = linspace(5, 90, 21) / 20  # volts  !!number must be odd!!
+hold_time = 5 * 60  # seconds
 
 
-displacements = append(copy(displacements[::2]),flip(copy(displacements[1::2])))
+displacements = append(copy(displacements[::2]), flip(copy(displacements[1::2])))
 print(displacements)
-data = array([displacements[0]/2]*int(hold_time/dt))
+data = array([displacements[0] / 2] * int(hold_time / dt))
 direction = -1
-num = len(ramp_rates)*len(displacements)
+num = len(ramp_rates) * len(displacements)
 i = 1
 for (ramp, disp) in product(ramp_rates, displacements):
-    print("Progress: %5.2f%%" % (i/num*100))
+    print("Progress: %5.2f%%" % (i / num * 100))
     i += 1
-    #print("Ramp: %.2f v/s  Disp: %.2f v" % (ramp, disp))
+    # print("Ramp: %.2f v/s  Disp: %.2f v" % (ramp, disp))
     start_v = data[-1]
-    data = append(data, linspace(start_v, start_v + direction*disp, int(disp/ramp/dt)))
-    data = append(data, linspace(data[-1], data[-1], int(hold_time/dt)))
+    data = append(
+        data, linspace(start_v, start_v + direction * disp, int(disp / ramp / dt))
+    )
+    data = append(data, linspace(data[-1], data[-1], int(hold_time / dt)))
     direction *= -1
 
-wavfile.write("wavegen.wav",int(1/dt),data)
+wavfile.write("wavegen.wav", int(1 / dt), data)
 print("Wav File Written")
 
 # plt.plot(linspace(0, len(data)*dt/60/60, len(data)),data*20)
 # plt.axhline(50, linewidth=1, color="black")
 # plt.axhline(-50, linewidth=1, color="black")
 # plt.show()
-
