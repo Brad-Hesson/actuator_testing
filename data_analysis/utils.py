95ade8bbad77c4865393abd0ebf292a52a1c7c56
Brad Hesson <brad.hesson@outlook.com>
2021-07-12 15:36:29 -0600

    formatted and added interp_zero_crossing fn

--- data_analysis/utils.py
+++ data_analysis/utils.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import re
 
+
 def get_files_in_dir(path, full_path=False):
     iter = os.walk(path)
     path, _, fnames = next(iter)
@@ -9,6 +10,7 @@ def get_files_in_dir(path, full_path=False):
         return [os.path.normpath(os.path.join(path, fname)) for fname in fnames]
     else:
         return [os.path.normpath(fname) for fname in fnames]
+
 
 def read_data_file(path):
     pat = re.compile("^((-?\d+(\.\d+)?(e-?\d+)?),?)+$")
@@ -18,12 +20,19 @@ def read_data_file(path):
             if pat.match(line) is not None:
                 data_start = i
                 break
-    return np.loadtxt(path, delimiter=',', skiprows=data_start, dtype=np.float64)
+    return np.loadtxt(path, delimiter=",", skiprows=data_start, dtype=np.float64)
+
+
+def get_interp_zero_crossing(x, y):
+    zci = np.nonzero(np.diff(np.sign(y)))[0][0]
+    m = (y[zci + 1] - y[zci]) / (x[zci + 1] - x[zci])
+    return (m * x[zci] - y[zci]) / m
+
 
 if __name__ == "__main__":
     DEBUG = False
     folder = "data/sn0001"
-    pred = lambda p: '.csv' in p
+    pred = lambda p: ".csv" in p
     files = filter(pred, get_files_in_dir(folder, True))
     data = read_data_file(list(files)[0])
-    print(data[0,2])
+    print(data[0, 2])
