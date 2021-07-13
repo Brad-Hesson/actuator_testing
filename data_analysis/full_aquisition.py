9e512b1f682ff69a3899ee592f00451eae5145cd
Brad Hesson <brad.hesson@outlook.com>
2021-07-13 14:25:15 -0600

    created full_aquisition file

---
+++ data_analysis/full_aquisition.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import get_files_in_dir, read_data_file
+from meta_profile import get_aquisition_datetime, get_meta_profile, get_aquisition_time_vector
+
+if __name__ == "__main__":
+    folder = "data/sn0001"
+    folder = r"data\old\4plate_v1\P10min_A100m_S1p"
+    
+    ts = get_aquisition_time_vector(folder)
+    fs = get_files_in_dir(folder)
+
+    all_data = np.empty((0,2))
+    num = len(fs)
+    for f, t, i in zip(fs, ts, range(len(fs))):
+        print("Full aquisition construction: %6.2f%% complete" % (100*(i+1)/num))
+        data = read_data_file(f)[:,[0,1]]
+        data[:,0] += t
+        all_data = np.vstack((all_data, data))
+    
+    vs = get_meta_profile(folder)
+
+    plt.subplot(2,1,1)
+    plt.plot(all_data[:,0], all_data[:,1])
+    plt.plot(vs[:,0], vs[:,1])
+
+    plt.subplot(2,1,2)
+    all_data[:,1] -= np.interp(all_data[:,0], vs[:,0], vs[:,1])
+    plt.plot(all_data[:,0], all_data[:,1]*1000*1000*1000)
+    plt.show()
+
