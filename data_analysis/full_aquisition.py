116ad609d4fd18dc077880f275c7459ca7246c89
Brad Hesson <brad.hesson@outlook.com>
2021-07-13 16:51:35 -0600

    changed import style

--- data_analysis/full_aquisition.py
+++ data_analysis/full_aquisition.py
@@ -1,32 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.lib import utils
-from utils import get_files_in_dir, read_data_file, cache_result
-from meta_profile import (
-    get_aquisition_datetime,
-    get_meta_profile,
-    get_aquisition_time_vector,
-)
+import utils
+import meta_profile
 
 
-@cache_result(ttl=60*10)
+@utils.cache_result(ttl=60*10)
 def get_full_aquisition(folder):
-    ts = get_aquisition_time_vector(folder)
-    fs = get_files_in_dir(folder)
+    ts = meta_profile.get_aquisition_time_vector(folder)
+    fs = utils.get_files_in_dir(folder)
 
     all_data = np.empty((0, 2))
     num = len(fs)
     for f, t, i in zip(fs, ts, range(len(fs))):
         print("Full aquisition construction: %6.2f%% complete" % (100 * (i + 1) / num))
-        data = read_data_file(f)[:, [0, 1]]
+        data = utils.read_data_file(f)[:, [0, 1]]
         data[:, 0] += t
         all_data = np.vstack((all_data, data))
     return all_data
 
-@cache_result(ttl=60*10)
+@utils.cache_result(ttl=60*10)
 def get_normalized_full_aquisition(folder):
     data = get_full_aquisition(folder)
-    vs = get_meta_profile(folder)
+    vs = meta_profile.get_meta_profile(folder)
     data[:, 1] -= np.interp(data[:, 0], vs[:, 0], vs[:, 1])
     return data
 
@@ -36,7 +31,7 @@ if __name__ == "__main__":
     #folder = r"data\old\4plate_v1\P10min_A100m_S1p"
 
     data = get_full_aquisition(folder)
-    vs = get_meta_profile(folder)
+    vs = meta_profile.get_meta_profile(folder)
 
     plt.subplot(2, 1, 1)
     plt.plot(data[:, 0], data[:, 1])
