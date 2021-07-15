b936e060814492d5647018e9964b4493328840a6
Brad Hesson <brad.hesson@outlook.com>
2021-07-14 20:01:43 -0600

    wrote several of the functions

--- data_analysis/data_fitting.py
+++ data_analysis/data_fitting.py
@@ -1,8 +1,19 @@
 import numpy as np
 import utils
 import meta_profile
+import full_aquisition
 import matplotlib.pyplot as plt
 import os
+import re
+from scipy import optimize
+
+
+def get_metadata(path):
+    folder = os.path.dirname(path)
+    fname = os.path.basename(path)
+    data = np.loadtxt(os.path.join(folder, "waveform.csv"))
+    ind = int(re.match(r"acq(\d+).csv", fname).groups()[0]) - 1
+    return (data[ind, 0], data[ind, 1])
 
 
 def get_normalized_data(path):
@@ -11,13 +22,44 @@ def get_normalized_data(path):
     data = utils.read_data_file(path)
     meta = meta_profile.get_meta_profile(folder)
     data[:, 1] -= np.interp(data[:, 0] + t, meta[:, 0], meta[:, 1])
-    return data[:, [0, 1]]
+    return data
+
+
+def get_aligned_data(path, normalize=True):
+    if normalize:
+        data = get_normalized_data(path)
+    else:
+        data = utils.read_data_file(path)
+    _, t1 = get_ramp_corner_times(path)
+    start_ind = utils.get_index_zero_crossings(data[:, 0] - t1)[0] + 1
+    data[:, 1] *= meta_profile.get_crossing_directions(data[:, 2])
+    data = data[start_ind:, :]
+    data[:, 1] -= data[0, 1]
+    data[:, 0] -= t1
+    return data
+
+
+def get_ramp_corner_times(path):
+    data = get_normalized_data(path)
+    m, _ = get_metadata(path)
+    m *= meta_profile.get_crossing_directions(data[:, 2])[0]
+
+    def f(x, x1, x2, y2):
+        b = y2 - m * x2
+        y1 = y2 - m * (x2 - x1)
+        return np.piecewise(
+            x, [x <= x1, x1 < x, x2 <= x], [y1, lambda x: m * x + b, y2]
+        )
+
+    p0 = [-0.1, 0.1, data[-1, 2]]
+    out = optimize.curve_fit(f, data[:, 0], data[:, 2], p0)
+    return (out[0][0], out[0][1])
 
 
 if __name__ == "__main__":
-    path = "data/sn0001/07-12-2021/acq0100.csv"
+    path = "data/sn0001/07-14-2021/acq0004.csv"
 
-    data = get_normalized_data(path)
+    data = get_aligned_data(path, False)
 
     plt.plot(data[:, 0], data[:, 1])
     plt.show()
