9390f037f9760e2781103d1b8a649cec0dd710b9
Brad Hesson <brad.hesson@outlook.com>
2021-07-16 20:47:54 -0600

    progress on data fitting i guess

--- data_analysis/data_fitting.py
+++ data_analysis/data_fitting.py
@@ -5,7 +5,7 @@ import full_aquisition
 import matplotlib.pyplot as plt
 import os
 import re
-from scipy import optimize
+from scipy import interpolate, optimize, signal, ndimage, stats
 
 
 def get_metadata(path):
@@ -67,10 +67,77 @@ def get_spline_data(path):
     return spl
 
 
+def logtimize(path):
+    xs = utils.read_data_file(path)[:, 0]
+    spline = get_spline_data(path)
+    ys = spline.derivative(1)(xs)
+
+    def f(x):
+        lxs = np.log(xs + x[0])
+        lys = np.log(ys + x[1])
+        mask = ~np.isnan(lxs) & ~np.isnan(lys)
+        slope, intercept, r_value, p_value, std_err = stats.linregress(lxs[mask], lys[mask])
+        print(slope, intercept)
+        plt.plot(np.log(xs + x[0]), np.log(ys + x[1]))
+        plt.plot(np.log(xs + x[0]), slope*np.log(xs + x[0])+intercept)
+        plt.show()
+        return std_err
+
+    res = optimize.minimize(f, [0,0])
+    print(res)
+
+
 if __name__ == "__main__":
-    path = "data/sn0001/07-14-2021/acq0004.csv"
+    plt.style.use(["dark_background", "seaborn-deep"])
+    folder = "data/sn0001/07-15-2021"
+    paths = utils.get_data_files_in_dir(folder)
+    last_metadata = get_metadata(paths[0])
+    ax1 = plt.subplot(2, 1, 1)
+    ax2 = plt.subplot(2, 1, 2)
+    for path in paths:
+        metadata = get_metadata(path)
+        if last_metadata != metadata:
+            figManager = plt.get_current_fig_manager()
+            figManager.window.showMaximized()
+            plt.tight_layout()
+            plt.show()
+            ax1 = plt.subplot(2, 1, 1)
+            ax2 = plt.subplot(2, 1, 2)
+        last_metadata = metadata
 
-    data = get_aligned_data(path, False)
+        data = utils.read_data_file(path)
+        mid = (np.max(data[:, 1]) + np.min(data[:, 1])) / 2
+        direction = meta_profile.get_crossing_directions(data[:, 1] - mid)[0]
+        color = "green" if direction > 0 else "red"
 
-    plt.plot(data[:, 0], data[:, 1])
-    plt.show()
+        data = get_aligned_data(path, False)
+
+        spl = get_spline_data(path)
+
+        ax1.set_title(path)
+        ax1.plot(data[:, 0], data[:, 1], color=color, alpha=0.4)
+        ax1.plot(data[:, 0], spl(data[:, 0]), color=color)
+
+        xs = data[:, 0]
+        ys = spl.derivative(1)(xs)
+        ax2.plot(np.log(xs + 0.5), np.log(ys+1e-11), color=color)
+        # ax2.set_ylim(-1e-10, 1e-9)
+
+        # def f(x, a, w, b, c):
+        #     return a * np.power(x + w, -b) + c
+
+        # xs = np.linspace(20, data[-1, 0], 1600)
+        # try:
+        #     out = optimize.curve_fit(
+        #         f,
+        #         xs,
+        #         spl.derivative(1)(xs),
+        #     )
+        #     a = out[0][0]
+        #     w = out[0][1]
+        #     b = out[0][2]
+        #     c = out[0][3]
+
+        #     plt.plot(xs, f(xs, *out[0]), "--")
+        # except RuntimeError:
+        #     pass
